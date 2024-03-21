4cac1d5326108f9e39c8705fcdc7c0524b4f7a34
Rod Mastria <rodmastria@gmail.com>
2024-03-21 12:18:23 -0400

    WorkInProgress Quiz/DB logic

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { QuizModel } from '../../Models/quiz-model';
 import { QuizService } from '../../services/quiz.service';
+import { FavService } from '../../services/fav.service';
+import { FavModel } from '../../Models/fav-model';
 
 @Component({
   selector: 'app-quiz',
@@ -11,26 +13,54 @@ import { QuizService } from '../../services/quiz.service';
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
-AllQuestions: QuizModel[] = [];
-QuestionForm: QuizModel = {} as QuizModel
+    AllQuestions: QuizModel[] = [];
+    QuestionForm: QuizModel = {} as QuizModel
 
-displayanswer: boolean = false
+    showHideAnswer: boolean = false
+    selectedQuiz: QuizModel = {} as QuizModel
 
-constructor (private quizService: QuizService) {}
+    @Output() createEvent = new EventEmitter<FavModel>();
 
-ngOnInit() {
-  this.quizService.GetAll().subscribe((response: QuizModel[]) => {
-   this.AllQuestions = response; 
-  })
-}
+    constructor (private quizService: QuizService, private favService: FavService) {}
 
-deleteQuestion(q: QuizModel) {
-  let index: number = this.AllQuestions.findIndex(x => x == q)
-  this.AllQuestions.splice(index,1);
-  this.quizService.DeleteQuestion(q).subscribe()
-}
+    ngOnInit() {
+      this.quizService.GetAll().subscribe((response: QuizModel[]) => {
+      this.AllQuestions = response; 
 
-AddQuestion(q: QuizModel):void {
-  this.AllQuestions.push(q)
-};
+      console.log("QUESTIONS " + this.AllQuestions[0]);
+      })
+
+      this.favService.GetAll().subscribe((response: FavModel[]) => {
+
+  
+        console.log("Faves " + response);
+        })
+
+    }
+
+    deleteQuestion(q: QuizModel) {
+      let index: number = this.AllQuestions.findIndex(x => x == q)
+      this.AllQuestions.splice(index,1);
+      this.quizService.DeleteQuestion(q).subscribe()
+    }
+
+    AddQuestion(q: QuizModel):void {
+      this.AllQuestions.push(q)
+    };
+
+    DisplayAnswer(q: QuizModel){
+      this.selectedQuiz = q;
+      this.showHideAnswer = !this.showHideAnswer
+    }
+
+    // AddFavorite(q: FavModel){
+    //   this.favService.AddFavorite(q).subscribe((fave : FavModel) => {
+    //     this.createEvent.emit(fave);
+    //   });
+    // }
+
+    // AddOrder():void {
+    //   this.orderService.addOrder(this.formOrder).subscribe((response:Order) => {
+    //     this.createEvent.emit(response);
+    //   } )
 }
