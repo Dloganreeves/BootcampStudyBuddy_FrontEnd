0fdd03d34fb1d931d8f8f77718d3c2a3d1613cb1
Rod Mastria <rodmastria@gmail.com>
2024-03-23 12:28:48 -0400

    Updates to Google login behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { FormComponent } from './components/form/form.component';
 import { LoginComponent } from './components/login/login.component';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,21 @@ import { LoginComponent } from './components/login/login.component';
 })
 export class AppComponent {
   title = 'BootCampStudyBuddy_FrontEnd';
+  loggedIn: boolean = false;
+  user: SocialUser = {} as SocialUser;
+
+  constructor (private socialAuthServiceConfig: SocialAuthService) {}
+
+  ngOnInit() {
+    this.socialAuthServiceConfig.authState.subscribe((userResponse: SocialUser) => {
+      this.user = userResponse;
+      //if login fails, it will return null.
+      this.loggedIn = (userResponse != null);
+    });
+  }
+
+  signOut(): void {
+    this.socialAuthServiceConfig.signOut();
+  }
+
 }
--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -6,7 +6,7 @@ import { FavService } from '../../services/fav.service';
 import { FavModel } from '../../Models/fav-model';
 import { FavDtoModel } from '../../Models/fav-dto-model';
 import { FormComponent } from '../form/form.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
@@ -26,16 +26,22 @@ export class QuizComponent {
     loggedIn: boolean = false;
   
   
-    constructor (private quizService: QuizService, private favService: FavService, private socialAuthServiceConfig: SocialAuthService) {}
+    constructor (private quizService: QuizService, private favService: FavService, 
+      private socialAuthServiceConfig: SocialAuthService, private router: Router) {}
 
     ngOnInit() {
       this.quizService.GetAll().subscribe((response: QuizModel[]) => {
-      this.AllQuestions = response; 
+        this.AllQuestions = response; 
       })
       this.socialAuthServiceConfig.authState.subscribe((userResponse: SocialUser) => {
         this.user = userResponse;
         //if login fails, it will return null.
         this.loggedIn = (userResponse != null);
+
+        if (!this.loggedIn){
+          this.router.navigate(["/"]); // send user to login screen
+        }
+
       });
     }
 
